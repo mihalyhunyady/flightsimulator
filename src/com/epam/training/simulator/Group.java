9088b66dcb2850ffd061d51385a8cbf28b36a1e5
Mihály Hunyady <megamegax@gmail.com>
2016-05-23 14:53:50 +0200

    nagyon kész

--- src/com/epam/training/simulator/Group.java
+++ src/com/epam/training/simulator/Group.java
@@ -8,8 +8,19 @@ import java.util.Random;
 public class Group {
     List<Passenger> passengerList;
 
+    public Group(Passenger passenger) {
+        passengerList = new ArrayList<>();
+        passengerList.add(passenger);
+    }
+
+    public Group(List<Passenger> passengers) {
+        passengerList = new ArrayList<>();
+        passengerList.addAll(passengers);
+    }
+
     public Group(int numberOfPassengers) {
         passengerList = new ArrayList<>();
+
         Random rnd = new Random();
         for (int i = 0; i < numberOfPassengers; i++) {
             passengerList.add(new Passenger(rnd.nextInt(10) + 1));

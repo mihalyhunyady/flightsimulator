40f3e1882778c56394b9c0db2817af20becf1ef1
Laszlo_Konya <laszlo_konya@epam.com>
2016-05-23 13:17:03 +0200

    classes created

---
+++ src/com/epam/training/simulator/FrequentFlyerState.java
@@ -0,0 +1,7 @@
+package com.epam.training.simulator;
+
+/**
+ * Created by Laszlo_Konya on 5/23/2016.
+ */
+public class FrequentFlyerState {
+}
---
+++ src/com/epam/training/simulator/Passenger.java
@@ -0,0 +1,8 @@
+package com.epam.training.simulator;
+
+/**
+ * Created by Laszlo_Konya on 5/23/2016.
+ */
+public class Passenger {
+    private int ID;
+}

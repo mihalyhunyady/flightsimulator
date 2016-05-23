5bf080c167d06186d91c8a83c0a1604e688512bf
Mihály Hunyady <megamegax@gmail.com>
2016-05-23 14:07:48 +0200

    ár számítás

--- src/com/epam/training/simulator/Trip.java
+++ src/com/epam/training/simulator/Trip.java
@@ -1,5 +1,36 @@
 package com.epam.training.simulator;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Trip {
+
+    Group group;
+    Itinerary itinerary;
+    List<Ticket> ticketList;
+
+    public Trip(Group group, Itinerary itinerary, List<Ticket> tickets) {
+        this.itinerary = itinerary;
+        this.ticketList = tickets;
+        this.group = group;
+    }
+
+    public double fare() {
+        List<Double> tempPrices = new ArrayList<>();
+        for (Passenger p : group.passengerList) {
+            tempPrices.addAll(ticketList.stream().map(ticket -> p.fFState.getDiscount() * ticket.getPrice()).collect(Collectors.toList()));
+        }
+        return tempPrices.stream().mapToDouble(a -> a).average().getAsDouble();
+        /*
+         for (Passenger p : group.passengerList) {
+            for (Ticket ticket : ticketList) {
+                tempPrices.add(p.fFState.getDiscount() * ticket.getPrice());
+            }
+        }
+         */
+    }
+
+
 }
